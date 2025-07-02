2952ece841e9aa4c13b2834f1e1aec8218a2c61e
upfall-james <upfall.james.synn@gmail.com>
2025-07-02 10:56:10 +0900

    Deploy website - based on 821b5d2ab89d7f13e83c809229228b88c4317a98

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/james/Projects/upfall/voidx-ai-docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/james/Projects/upfall/voidx-ai-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/james/Projects/upfall/voidx-ai-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/james/Projects/upfall/voidx-ai-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/james/Projects/upfall/voidx-ai-docs/src/css/custom.css"),
-];
